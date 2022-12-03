a33c275731f28628b64504385486fb9f57785978
Juan Diego Castellanos Jerez <juancastellanosj15@gmail.com>
2022-12-03 17:26:52 -0500

    Admin class completed

--- app/src/main/java/com/example/appfondosfinancieros/models/Admin.java
+++ app/src/main/java/com/example/appfondosfinancieros/models/Admin.java
@@ -1,4 +1,80 @@
 package com.example.appfondosfinancieros.models;
 
 public class Admin {
+    private String name;
+    private String lastName;
+    private String identificationType;
+    private int identificationNumber;
+    private String email;
+    private String phoneNumber;
+    private String password;
+
+    public Admin() {
+    }
+
+    public Admin(String name, String lastName, String identificationType, int identificationNumber, String email, String phoneNumber, String password) {
+        this.name = name;
+        this.lastName = lastName;
+        this.identificationType = identificationType;
+        this.identificationNumber = identificationNumber;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIdentificationType() {
+        return identificationType;
+    }
+
+    public void setIdentificationType(String identificationType) {
+        this.identificationType = identificationType;
+    }
+
+    public int getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public void setIdentificationNumber(int identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
