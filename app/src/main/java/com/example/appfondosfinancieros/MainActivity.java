131cd966d557caf16dd1cab434b151f8446a35c2
Juan Diego Castellanos Jerez <juancastellanosj15@gmail.com>
2022-12-07 12:42:01 -0500

    Arreglando activites

--- app/src/main/java/com/example/appfondosfinancieros/MainActivity.java
+++ app/src/main/java/com/example/appfondosfinancieros/MainActivity.java
@@ -5,18 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnIniciarCajero, btnIniciarParticipante;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIniciarCajero = findViewById(R.id.btnIniciarCajero);
+        btnIniciarParticipante = findViewById(R.id.btnIniciarParticipante);
+
+        btnIniciarCajero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarSesionCajero(v);
+            }
+        });
+
     }
 
     public void iniciarSesionCajero(View view){
         Intent inisesCajero = new Intent(MainActivity.this,iniciarSesionCajeroActivity.class);
         startActivity(inisesCajero);
     }
-
 }
