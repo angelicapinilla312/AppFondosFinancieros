ddafc08bb3b53b0eaf95fe2d14b68f5c2eae6a4f
Juan Diego Castellanos Jerez <juancastellanosj15@gmail.com>
2022-12-11 02:27:26 -0500

    Activities completed

--- app/src/main/java/com/example/appfondosfinancieros/InputCode.java
+++ app/src/main/java/com/example/appfondosfinancieros/InputCode.java
@@ -2,13 +2,39 @@ package com.example.appfondosfinancieros;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class InputCode extends AppCompatActivity {
+
+    ImageView imgVwOkayCode;
+    EditText edtxCode;
+
+    final String CODE = "xdrs-2231-rtf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_code);
+
+        imgVwOkayCode = findViewById(R.id.imgVwCodeOkay);
+        edtxCode = findViewById(R.id.edtxCode);
+
+        imgVwOkayCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validarCodigo();
+            }
+        });
+    }
+
+    public void validarCodigo(){
+        if(edtxCode.getText().toString() == CODE){
+            Intent welcomeParticipants = new Intent(InputCode.this,WelcomeParticipants.class);
+            startActivity(welcomeParticipants);
+        }
     }
 }
--- app/src/main/java/com/example/appfondosfinancieros/WelcomeParticipants.java
+++ app/src/main/java/com/example/appfondosfinancieros/WelcomeParticipants.java
@@ -2,13 +2,31 @@ package com.example.appfondosfinancieros;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeParticipants extends AppCompatActivity {
+
+    Button btnWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_participants);
+
+        btnWelcome = findViewById(R.id.btnWelcome);
+
+        btnWelcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToHome();
+            }
+        });
+    }
+    public void goToHome(){
+        Intent home = new Intent(WelcomeParticipants.this,HomeParticipant.class);
+        startActivity(home);
     }
 }
