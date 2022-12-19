9f6cf0d3bdcb08a8131ab79d941b479b9a410c0a
Juan Diego Castellanos Jerez <juancastellanosj15@gmail.com>
2022-12-18 20:39:46 -0500

    First version completed

--- app/src/main/java/com/example/appfondosfinancieros/SeguimientoCajero.java
+++ app/src/main/java/com/example/appfondosfinancieros/SeguimientoCajero.java
@@ -1,15 +1,35 @@
 package com.example.appfondosfinancieros;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.appfondosfinancieros.domain.entities.Prestamos;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SeguimientoCajero extends AppCompatActivity {
 
-    ImageView btnBackSeguimientoHome;
+    Button btnBackSeguimientoHome;
+    ListView lvPrestamos;
+    private List<Prestamos> prestamosList = new ArrayList<>();
+    ArrayAdapter<Prestamos> prestamosArrayAdapter;
+
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +37,41 @@ public class SeguimientoCajero extends AppCompatActivity {
         setContentView(R.layout.activity_seguimiento_cajero);
 
         btnBackSeguimientoHome = findViewById(R.id.btnBackSeguimientoHome);
+        lvPrestamos = findViewById(R.id.lvPrestamos);
+        inicializarFirebase();
+        listarDatos();
 
-        btnBackSeguimientoHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                regresarMenuHistorico(v);
-            }
-        });
+        btnBackSeguimientoHome.setOnClickListener(this::regresarMenuHistorico);
     }
 
     public void regresarMenuHistorico(View view){
         Intent backh = new Intent(SeguimientoCajero.this,MenuCajero.class);
         startActivity(backh);
+    }
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+        Toast.makeText(this, "Inicializando base de datos",
+                Toast.LENGTH_SHORT).show();
+    }
+    private void listarDatos() {
+        databaseReference.child("prestamo").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                prestamosList.clear();
+                for (DataSnapshot objSnapshot : snapshot.getChildren()) {
+                    Prestamos prestamos = objSnapshot.getValue(Prestamos.class);
+                    prestamosList.add(prestamos);
+                    prestamosArrayAdapter = new ArrayAdapter<Prestamos>(
+                            SeguimientoCajero.this, android.R.layout.simple_list_item_1,
+                            prestamosList);
+                    lvPrestamos.setAdapter(prestamosArrayAdapter);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
     }
 }
